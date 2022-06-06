2cac8624b51d42f81d3745f3cd779e59f01c721d
Artur <am29-10@mail.ru>
2022-06-06 15:26:48 +0300

    fix: Реализовать новый вариант тестирования

--- src/Main.java
+++ src/Main.java
@@ -10,36 +10,42 @@ public class Main {
     public static void main(String[] args) {
         TaskManager manager = Managers.getDefault();
 
-        Task task1 = new Task("Утренняя пробежка", "Бег на стадионе", TaskStatus.NEW);
-        manager.createTask(task1);
-        Task task2 = new Task("Сходить в магазин", "Покупка продуктов", TaskStatus.NEW);
-        manager.createTask(task2);
-
         Epic epic = new Epic("Сделать уроки", "Написать конспекты", TaskStatus.NEW);
         manager.createEpic(epic);
         SubTask subTaskForEpic1 = new SubTask("Сделать алгебру", "Конспект по логарифмам", TaskStatus.IN_PROGRESS, 1);
         manager.createSubTask(subTaskForEpic1);
         SubTask subTaskForEpic2 = new SubTask("Сделать геометрию", "Конспект по теореме Пифагора", TaskStatus.NEW, 1);
         manager.createSubTask(subTaskForEpic2);
+        SubTask subTaskForEpic3 = new SubTask("Сделать физику", "Конспект по закону Ома", TaskStatus.NEW, 1);
+        manager.createSubTask(subTaskForEpic3);
 
         Epic epic1 = new Epic("Прочитать книжку", "Чтение в библиотеке", TaskStatus.NEW);
         manager.createEpic(epic1);
-        SubTask subTaskForEpic11 = new SubTask("Прочитать Достоевского", "Преступление и наказание", TaskStatus.NEW, 2);
-        manager.createSubTask(subTaskForEpic11);
 
-
-        System.out.println(manager.getTask(1));
-        System.out.println(manager.getTask(2));
-        System.out.println(manager.getEpic(3));
-        System.out.println(manager.getEpic(6));
+        System.out.println("Получаем задачи:");
+        System.out.println(manager.getEpic(1));
+        System.out.println(manager.getEpic(5));
+        System.out.println(manager.getSubTask(2));
+        System.out.println(manager.getSubTask(3));
         System.out.println(manager.getSubTask(4));
-        System.out.println(manager.getTask(1));
-        System.out.println(manager.getTask(2));
-        System.out.println(manager.getEpic(6));
-        System.out.println(manager.getEpic(3));
-        System.out.println(manager.getSubTask(7));
-        System.out.println("---------------------------------------");
-        System.out.println(manager.getHistory());
 
+        System.out.println("Получаем имеющиеся задачи (проверка обновления истории):");
+        System.out.println(manager.getEpic(1));
+        System.out.println(manager.getSubTask(2));
+
+        System.out.println("Вывод на экран списка истории: ");
+        System.out.println(manager.getHistory());
+        System.out.println(manager.getHistory().size());
+
+        System.out.println("Удаляем задачу (проверка обновления истории):");
+        manager.removeSubTask(2);
+        System.out.println(manager.getHistory());
+        System.out.println(manager.getHistory().size());
+
+        System.out.println("Удаляем эпик в котором есть подзадачи (проверка удаления как самого эпика, так и его " +
+                "подзадач из истории):");
+        manager.removeEpic(1);
+        System.out.println(manager.getHistory());
+        System.out.println(manager.getHistory().size());
     }
 }
